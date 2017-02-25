b0aea5d23e35a829f32b90f6a705e5640cd4be16
Ognjen Jevremovic <ognjen.jevremovic@sbgenomics.com>
2017-02-25 11:07:42 +0100

    bugfix

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const {  }
-
 router.get('/', (req, res) => {
     if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'alexander') {
         console.log("Validating webhook");
