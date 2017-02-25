5728f0da3ea98901cdbfc36ed48f43f3158c6b87
Ognjen Jevremovic <ognjen.jevremovic@sbgenomics.com>
2017-02-25 14:06:08 +0100

    bugfix

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -105,16 +105,20 @@ function updateMessage(input, response) {
 }
 */
 
-router.post('/webhook/', function (req, res) {
+router.post('/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
+
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
+        
         if (event.message && event.message.text) {
             let text = event.message.text
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
+
     }
+
     res.sendStatus(200)
 })
 
