a0edd89ecb0c729cba931e7af3ac859b46d0fda1
Ognjen Jevremovic <ognjen.jevremovic@sbgenomics.com>
2017-02-25 15:39:01 +0100

    Other approach

--- src/helpers/returnResponse.js
+++ src/helpers/returnResponse.js
@@ -21,11 +21,19 @@ module.exports = (sender, text) => {
                 text: text
             },
         }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
+    }, function (error, response, body) {
+        //  Successfuly returned the message
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            console.log("Successfully sent generic message with id %s to recipient %s",
+                messageId, recipientId);
+        //  Error responding to user
+        } else {
+            console.error("Unable to send message.");
+            console.error(response);
+            console.error(error);
         }
     });
 };
--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -35,7 +35,34 @@ router.get('/', (req, res) => {
 
 //  Webhook
 router.post('/', function (req, res) {
-    
+
+    //  Extract the request body
+    var data = req.body;
+
+    // Make sure this is a page subscription
+    if (data.object === 'page') {
+
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function (entry) {
+            var pageID = entry.id;
+            var timeOfEvent = entry.time;
+
+            // Iterate over each messaging event
+            entry.messaging.forEach(function (event) {
+                if (event.message) {
+                    receivedMessage(event);
+                } else {
+                    console.log("Webhook received unknown event: ", event);
+                }
+            });
+        });
+
+        //  Return the status code of 200
+        res.sendStatus(200);
+    }
+
+
+    /*
     //  Get events
     let messaging_events = req.body.entry[0].messaging;
 
@@ -65,8 +92,67 @@ router.post('/', function (req, res) {
         }
 
     }
-})
+    */
+});
 
+
+function receivedMessage(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+
+    console.log("Received message for user %d and page %d at %d with message:",
+        senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+
+    var messageId = message.mid;
+
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+
+    if (messageText) {
+
+        sendTextMessage(senderID, messageText);
+    } else if (messageAttachments) {
+        sendTextMessage(senderID, "Message with attachment received");
+    }
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
 
 
 //  Export the module
